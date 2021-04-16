3c6c6d238bc83ef723e61c4fea0de9dd2c3c14e7
brandong1 <brandon@brandonleeproductions.com>
2021-04-16 12:15:46 -0600

    Test regular print function

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	f "fmt"
+	"runtime"
+)
 
 func main() {
 	fmt.Println("Hi! I want to be a Gopher!")
-	fmt.Print("Hi! I want to be a Pickle!")
+	f.Println("Hi! I want to be a Pickle!")
+	f.Println(runtime.NumCPU())
 }
