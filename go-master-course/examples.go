1c62d08d22a21c8508ae4f37a51382fbf3c053ad
Brandon Green <brandon@brandonleeproductions.com>
2021-08-05 09:16:15 -0600

    add examples

---
+++ go-master-course/examples.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// var version string // declaration at the package level
+
+// func main() {
+// 	// score := 0 -- DON'T DO THIS if you don't know the initial value
+// 	var score int // score = 0
+
+// 	// Group related variables together for better readability:
+// 	var (
+// 		//related:
+// 		video string
+
+// 		//closely related
+// 		duration int
+// 		current  int
+// 	)
+// }
+// func main() {
+// 	color := "green" // short declaration ":=" to assign initial value
+
+// 	fmt.Println(color)
+
+// 	color = "blue" // use equal to re-assign variable with new value
+
+// 	fmt.Println(color)
+// }
+
+// func main() {
+// 	color := "green"
+
+// 	// `"dark " + color` is an expression
+
+// 	color = "dark " + color
+
+// 	fmt.Println(color) // output: darkgreen
+// }
---
+++ go-master-course/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var speed int //default value is 0
+
+//Declare multiple variables
+//var dir, file string
+//dir, file = path.Split("css/main.css")
+
+func main() {
+	fmt.Println(speed)
+}
